1150c4cea7cd1f26797859da6963f797a8230445
Aleksandra Wisniewska <wisniewska.aleks@gmail.com>
2016-09-02 13:38:20 +0100

    add trailing slash to URL

--- config/article.js
+++ config/article.js
@@ -5,7 +5,7 @@ export default _ => ({ // eslint-disable-line
 
   // canonical URL of the published page
   // https://ig.ft.com/sites/future-of-the-city get filled in by the ./configure script
-  url: 'https://ig.ft.com/sites/future-of-the-city',
+  url: 'https://ig.ft.com/sites/future-of-the-city/',
 
   // To set an exact publish date do this:
   //       new Date('2016-05-17T17:11:22Z')
